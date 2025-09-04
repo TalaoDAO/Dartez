12b01e53296e8140f3df01bee94111f5ef71b996
hawkbee1 <googandads@gmail.com>
2025-09-04 19:36:09 +0000

    migration to sodium_libs package

--- lib/utils/sodium/platform_impl/sodium_utils_io.dart
+++ lib/utils/sodium/platform_impl/sodium_utils_io.dart
@@ -1,19 +1,24 @@
+import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:argon2/argon2.dart';
 import 'package:dartez/utils/sodium/platform_impl/sodium_utils_base.dart';
-import 'package:sodium_libs/sodium_libs.dart' hide KeyPair;
+import 'package:sodium_libs/sodium_libs_sumo.dart' hide KeyPair;
 
 class SodiumUtilsImpl extends SodiumUtilsBase {
   late final Sodium sodium;
   @override
   Future<void> init() async {
-    sodium = await SodiumInit.init();
+    sodium = await SodiumSumoInit.init();
     return Future.value();
   }
 
   @override
   Uint8List close(Uint8List message, Uint8List nonce, Uint8List keyBytes) {
-    return sodium.crypto.secretBox.easy(message: message, nonce: nonce, key: SecureKey.fromList(sodium, keyBytes));
+    return sodium.crypto.secretBox.easy(
+        message: message,
+        nonce: nonce,
+        key: SecureKey.fromList(sodium, keyBytes));
   }
 
   @override
@@ -28,7 +33,10 @@ class SodiumUtilsImpl extends SodiumUtilsBase {
     final cipherText =
         nonceAndCiphertext.sublist(sodium.crypto.secretBox.nonceBytes);
 
-    return sodium.crypto.secretBox.openEasy(cipherText: cipherText, nonce: nonce, key: SecureKey.fromList(sodium, key));
+    return sodium.crypto.secretBox.openEasy(
+        cipherText: cipherText,
+        nonce: nonce,
+        key: SecureKey.fromList(sodium, key));
   }
 
   @override
@@ -42,52 +50,64 @@ class SodiumUtilsImpl extends SodiumUtilsBase {
 
   @override
   Uint8List pwhash(String passphrase, Uint8List salt) {
-    // return sodium.Sodium.cryptoPwhash(
-    //     sodium.Sodium.cryptoBoxSeedbytes,
-    //     Uint8List.fromList(passphrase.codeUnits),
-    //     salt,
-    //     4,
-    //     33554432,
-    //     sodium.Sodium.cryptoPwhashAlgArgon2i13);
-    throw UnimplementedError('Not implemented');
+    var parameters = Argon2Parameters(
+      Argon2Parameters.ARGON2_i,
+      salt,
+      version: Argon2Parameters.ARGON2_VERSION_13,
+      iterations: 4,
+      memoryPowerOf2: 16,
+    );
+    var argon2 = Argon2BytesGenerator();
 
+    argon2.init(parameters);
+
+    final passwordBytes = parameters.converter.convert(passphrase);
+
+    final result = Uint8List(32);
+    argon2.generateBytes(passwordBytes, result, 0, result.length);
+
+    return result;
   }
 
   @override
   Uint8List rand(int length) {
+    // Not used for now
     // return sodium.Sodium.randombytesBuf(length);
     throw UnimplementedError('Not implemented');
   }
 
   @override
   Uint8List salt() {
-    // return Uint8List.fromList(
-    //     rand(sodium.Sodium.cryptoPwhashSaltbytes).toList());
-    throw UnimplementedError('Not implemented');
+    final random = Random.secure();
+    return Uint8List.fromList(
+      List<int>.generate(32, (_) => random.nextInt(256)),
+    );
   }
 
   @override
   Uint8List sign(Uint8List simpleHash, Uint8List key) {
-    // return sodium.Sodium.cryptoSignDetached(simpleHash, key);
-    throw UnimplementedError('Not implemented');
+    return sodium.crypto.sign.detached(
+      message: simpleHash,
+      secretKey: SecureKey.fromList(sodium, key),
+    );
   }
 
   @override
   getSodium() {
-    // return sodium.Sodium;
-    throw UnimplementedError('Not implemented');
+    return sodium;
   }
 
   @override
   KeyPair cryptoSignSeedKeypair(Uint8List seed) {
-    // var temp = sodium.Sodium.cryptoSignSeedKeypair(seed);
-    // return KeyPair(temp.pk, temp.sk);
-    throw UnimplementedError('Not implemented');
+    var temp = sodium.crypto.sign.seedKeyPair(SecureKey.fromList(sodium, seed));
+    return KeyPair(temp.publicKey, temp.secretKey.extractBytes());
   }
 
   @override
   Uint8List cryptoSignDetached(Uint8List message, Uint8List key) {
-    // return sodium.Sodium.cryptoSignDetached(message, key);
-    throw UnimplementedError('Not implemented');
+    return sodium.crypto.sign.detached(
+      message: message,
+      secretKey: SecureKey.fromList(sodium, key),
+    );
   }
 }
