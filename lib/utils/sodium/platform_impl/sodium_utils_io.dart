d808bfeadcb28a7e6a892a20049c10123c9d71a8
hawkbee1 <googandads@gmail.com>
2025-09-04 08:13:42 +0000

    working on

--- lib/utils/sodium/platform_impl/sodium_utils_io.dart
+++ lib/utils/sodium/platform_impl/sodium_utils_io.dart
@@ -1,81 +1,93 @@
 import 'dart:typed_data';
 
 import 'package:dartez/utils/sodium/platform_impl/sodium_utils_base.dart';
-import 'package:flutter_sodium/flutter_sodium.dart' as sodium;
+import 'package:sodium_libs/sodium_libs.dart' hide KeyPair;
 
 class SodiumUtilsImpl extends SodiumUtilsBase {
+  late final Sodium sodium;
   @override
   Future<void> init() async {
-    return sodium.Sodium.init();
+    sodium = await SodiumInit.init();
+    return Future.value();
   }
 
   @override
   Uint8List close(Uint8List message, Uint8List nonce, Uint8List keyBytes) {
-    return sodium.Sodium.cryptoSecretboxEasy(message, nonce, keyBytes);
+    return sodium.crypto.secretBox.easy(message: message, nonce: nonce, key: SecureKey.fromList(sodium, keyBytes));
   }
 
   @override
   Uint8List nonce() {
-    return rand(sodium.Sodium.cryptoBoxNoncebytes);
+    return rand(sodium.crypto.box.nonceBytes);
   }
 
   @override
   Uint8List open(Uint8List nonceAndCiphertext, Uint8List key) {
-    var nonce =
-        nonceAndCiphertext.sublist(0, sodium.Sodium.cryptoSecretboxNoncebytes);
-    var ciphertext =
-        nonceAndCiphertext.sublist(sodium.Sodium.cryptoSecretboxNoncebytes);
+    final nonce =
+        nonceAndCiphertext.sublist(0, sodium.crypto.secretBox.nonceBytes);
+    final cipherText =
+        nonceAndCiphertext.sublist(sodium.crypto.secretBox.nonceBytes);
 
-    return sodium.Sodium.cryptoSecretboxOpenEasy(ciphertext, nonce, key);
+    return sodium.crypto.secretBox.openEasy(cipherText: cipherText, nonce: nonce, key: SecureKey.fromList(sodium, key));
   }
 
   @override
   KeyPair publicKey(Uint8List sk) {
-    var seed = sodium.Sodium.cryptoSignEd25519SkToSeed(sk);
-    var temp = sodium.Sodium.cryptoSignSeedKeypair(seed);
-    return KeyPair(temp.pk, temp.sk);
+    /// TODO(hawkbee): looks like it is not used => throwing error and commenting the old code for now
+    // var seed = sodium.Sodium.cryptoSignEd25519SkToSeed(sk);
+    // var temp = sodium.Sodium.cryptoSignSeedKeypair(seed);
+    // return KeyPair(temp.pk, temp.sk);
+    throw UnimplementedError('Not implemented');
   }
 
   @override
   Uint8List pwhash(String passphrase, Uint8List salt) {
-    return sodium.Sodium.cryptoPwhash(
-        sodium.Sodium.cryptoBoxSeedbytes,
-        Uint8List.fromList(passphrase.codeUnits),
-        salt,
-        4,
-        33554432,
-        sodium.Sodium.cryptoPwhashAlgArgon2i13);
+    // return sodium.Sodium.cryptoPwhash(
+    //     sodium.Sodium.cryptoBoxSeedbytes,
+    //     Uint8List.fromList(passphrase.codeUnits),
+    //     salt,
+    //     4,
+    //     33554432,
+    //     sodium.Sodium.cryptoPwhashAlgArgon2i13);
+    throw UnimplementedError('Not implemented');
+
   }
 
   @override
   Uint8List rand(int length) {
-    return sodium.Sodium.randombytesBuf(length);
+    // return sodium.Sodium.randombytesBuf(length);
+    throw UnimplementedError('Not implemented');
   }
 
   @override
   Uint8List salt() {
-    return Uint8List.fromList(
-        rand(sodium.Sodium.cryptoPwhashSaltbytes).toList());
+    // return Uint8List.fromList(
+    //     rand(sodium.Sodium.cryptoPwhashSaltbytes).toList());
+    throw UnimplementedError('Not implemented');
   }
 
   @override
   Uint8List sign(Uint8List simpleHash, Uint8List key) {
-    return sodium.Sodium.cryptoSignDetached(simpleHash, key);
+    // return sodium.Sodium.cryptoSignDetached(simpleHash, key);
+    throw UnimplementedError('Not implemented');
   }
 
   @override
   getSodium() {
-    return sodium.Sodium;
+    // return sodium.Sodium;
+    throw UnimplementedError('Not implemented');
   }
 
   @override
   KeyPair cryptoSignSeedKeypair(Uint8List seed) {
-    var temp = sodium.Sodium.cryptoSignSeedKeypair(seed);
-    return KeyPair(temp.pk, temp.sk);
+    // var temp = sodium.Sodium.cryptoSignSeedKeypair(seed);
+    // return KeyPair(temp.pk, temp.sk);
+    throw UnimplementedError('Not implemented');
   }
 
   @override
   Uint8List cryptoSignDetached(Uint8List message, Uint8List key) {
-    return sodium.Sodium.cryptoSignDetached(message, key);
+    // return sodium.Sodium.cryptoSignDetached(message, key);
+    throw UnimplementedError('Not implemented');
   }
 }
